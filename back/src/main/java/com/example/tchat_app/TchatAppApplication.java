6abfdddd2dc9e42180a404b0f2991a215fec7360
Thomas <thomas.mirabile@ynov.com>
2024-11-15 09:59:46 +0100

    Initial commit

---
+++ back/src/main/java/com/example/tchat_app/TchatAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.tchat_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TchatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TchatAppApplication.class, args);
+	}
+
+}
